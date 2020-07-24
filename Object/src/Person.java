4924ecc1112a6a12980e90f87ef08d7d8c319d8a
SL <susanavlr@live.cl>
2020-07-24 18:44:02 -0400

    tarea

---
+++ Object/src/Person.java
@@ -0,0 +1,14 @@
+
+public class Person {
+    private int age;
+    private String name;
+    public Person(int ageParam, String nameParam) {
+        this.age = ageParam;
+        this.name = nameParam;
+    }
+    public void objectMethods(Person anotherObject) {
+        System.out.println("getClass: " + this.getClass().getSimpleName());
+        System.out.println("toString: " + this.toString());
+        System.out.println("Equals: " + this.equals(anotherObject));
+    }
+}
---
+++ Object/src/PersonTest.java
@@ -0,0 +1,11 @@
+
+public class PersonTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person person1 = new Person(10, "Persona1");
+        Person person2 = new Person(5, "Persona2");
+        person1.objectMethods(person2);
+	}
+
+}
